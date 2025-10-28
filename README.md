0fe598394e75d3547649f8ff04189b309090e0e7
hychaochao <hyz.chaochao@gmail.com>
2025-10-29 01:46:08 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# ThinkMorph: Emergent Properties in Multimodal Interleaved Chain-of-Thought Reasoning
+
+Official code for paper: ThinkMorph: Emergent Properties in Multimodal Interleaved Chain-of-Thought Reasoning
+
+The code is based on the bagel codebase, see [Bagel](https://github.com/ByteDance-Seed/Bagel) for details.
+
+Training scripts is provided in ./scripts
---
+++ scripts/train_thinkmorph.sh
@@ -0,0 +1,26 @@
+# Copyright 2025 Bytedance Ltd. and/or its affiliates.
+# SPDX-License-Identifier: Apache-2.0
+
+# replace the variables with your own
+torchrun \
+  --nnodes=$num_nodes \
+  --node_rank=$node_rank \
+  --nproc_per_node=8 \
+  --master_addr=$master_addr \
+  --master_port=$master_port \
+  train/pretrain_unified_navit.py \
+  --dataset_config_file ./data/configs/interleaved_reasoning.yaml \
+  --layer_module Qwen2MoTDecoderLayer \
+  --finetune_from_hf True \
+  --auto_resume True \
+  --finetune-from-ema True \
+  --resume_from $resume_from \
+  --results_dir $output_path \
+  --checkpoint_dir $ckpt_path \
+  --lr 1e-5 \
+  --num_worker 4 \
+  --max_latent_size 64  \
+  --max_num_tokens 32768 \
+  --mse_weight 1 \
+  --ce_weight 1 \
+  --total_steps 8000 \
